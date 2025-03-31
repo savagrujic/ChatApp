d8b99d05ed18f3af9cf9b1e17cffeaa941b5953e
savagrujic <savagrujic42@gmail.com>
2025-03-31 18:36:01 +0100

    added css to home page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,22 @@
-
+import { FaGoogle } from "react-icons/fa";
+import './css/Login.css'
 export default function() {
 
     return(
         <div className="wrapper">
+            <div className="left-container">
 
-        Cao
+            </div>
+            <div className="right-container">
+                <form>
+                    <input />
+                    <input />
+                    <p>Forgot Password</p>
+                    <button>Login</button>
+                    <p>or login with platforms</p>
+                    <FaGoogle />
+                </form>
+            </div>
         </div>
     )
 }
