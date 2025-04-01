9384eb2a0997620b76c56b55be3f1ddbd9738758
savagrujic <savagrujic42@gmail.com>
2025-04-01 09:23:08 +0100

    styled login page added buton h1 and vi

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,11 @@ export default function() {
     return(
         <div className="wrapper">
             <div className="left-container">
-
+                    <div className="centered">
+                    <h1>Hello, Welcome!</h1>
+                    <p>Don't have an account?</p>
+                    <button>Register</button>
+                    </div>
             </div>
             <div className="right-container">
                 <form>
