f4c4b50e97755d4b6c71af84e3ed01a91820d31a
savagrujic <savagrujic42@gmail.com>
2025-04-01 22:54:20 +0100

    added signup

---
+++ src/firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFPSS85CarluPWhh9mhBh0x_6oMt5KDUs",
+  authDomain: "chatapp-8e5c3.firebaseapp.com",
+  projectId: "chatapp-8e5c3",
+  storageBucket: "chatapp-8e5c3.firebasestorage.app",
+  messagingSenderId: "930890545279",
+  appId: "1:930890545279:web:616e3871a379693bb0e74a",
+  measurementId: "G-XKVF3QXJ61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app)
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,25 +1,42 @@
 import { FaGoogle } from "react-icons/fa";
 import './css/Register.css'
 import { useNavigate } from "react-router";
-
+import { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword, auth } from "firebase/auth";
 
 export default function() {
 
+    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmpassword, setConfirmpassword] = useState('')
+    const [canRegister, setCanRegister] = useState(false)
     let naviage = useNavigate()
 
     function hadnelanvigate() {
         naviage('/')
     }
+
+    function HandleRegister(e) {
+        e.preventDefault()
+        createUserWithEmailAndPassword(auth,email,password)
+        .then((userCredential) => {
+            const user = userCredential.user
+            console.log(user)
+        }).catch((err) => {
+            console.error(err)
+        })
+        
+    }
     return(
         <div className="wrapper">
             <div className="right-container">
-                <form>
+                <form onSubmit={HandleRegister}>
                     <h1>Registration</h1>
-                    <input className="input1" placeholder="Username" />
-                    <input className="input2" placeholder="Email"/>
-                    <input className="input1" placeholder="Password" />
-                    <input className="input2" placeholder="Confirm Password"/>
-                 
+                    <input className="input2"  required onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email"/>
+                    <input className="input1"  required onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+                    <input className="input2"  required onChange={(e) => setConfirmpassword(e.target.value)} type="password" placeholder="Confirm Password"/>
+                    {password != confirmpassword ? (<p style={{color:'red'}}>Password are not matching!</p> ): ''}
                     <button>Register</button>
                     <p>or registrate with platforms</p>
                     <FaGoogle />
