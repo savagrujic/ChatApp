02ee437f3fddd0f80ee11b634205057fa2afee48
savagrujic <savagrujic42@gmail.com>
2025-04-01 23:14:57 +0100

    added logout feature

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,8 +1,34 @@
 
+import { signOut } from "firebase/auth"
 import { auth } from "../firebase-config"
-
+import { useNavigate } from "react-router"
 
 export default function Chat() {
 
-    return(<div>{auth.currentUser.displayName}</div>)
+    const navigate = useNavigate()
+    function singOut() {
+        signOut(auth).then(() => {
+            navigate('/')
+        }).catch((error) => {
+            console.error(error)
+        })
+    }
+    return(
+    <div>
+     
+ 
+    <h2>Hello {auth.currentUser.displayName}!</h2>
+    <div className="messagebox">
+        Messages wil be Here
+    </div>
+        <input placeholder="Enter Your Messages Here" />
+        <button>Send</button>
+        <button onClick={singOut}>LogOut</button>
+  
+    
+    
+    </div>
+
+
+)
 }
