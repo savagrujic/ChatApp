fe96cba2cbbad40876d82e3337ec0eeb8caae896
savagrujic <savagrujic42@gmail.com>
2025-04-02 10:12:32 +0100

    added chat

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,10 +1,14 @@
 
 import { signOut } from "firebase/auth"
-import { auth } from "../firebase-config"
+import { auth,db } from "../firebase-config"
 import { useNavigate } from "react-router"
+import { useEffect, useState } from "react"
+import { collection, addDoc,doc,onSnapshot, query, serverTimestamp, orderBy } from "firebase/firestore"
+
 
 export default function Chat() {
-
+    const [message, setMessage] = useState('')
+    const [messageList, setMessageList] = useState([])
     const navigate = useNavigate()
     function singOut() {
         signOut(auth).then(() => {
@@ -13,16 +17,50 @@ export default function Chat() {
             console.error(error)
         })
     }
-    return(
+    const collectionreference = collection(db,'messages')
+    const SendMessage = async () => {
+        try{
+        await addDoc((collectionreference), {
+            message,
+            author: {name: auth.currentUser.displayName, id: auth.currentUser.uid},
+            time: serverTimestamp()
+        })
+        }
+        catch(err) {
+            console.error(err)
+        }
+        setMessage('')
+    }
+
+    useEffect(()=> {
+        const q = query(collection(db,'messages'), orderBy('time', 'asc'))
+        const data = onSnapshot(q, (col) => {
+
+                const tempmessage = col.docs.map((mess) => ({
+                    message: mess.data().message,
+                    name: mess.data().author.name
+                }))
+               setMessageList(tempmessage)
+            
+        })
+    }, [])
+    
+    return( 
     <div>
      
  
     <h2>Hello {auth.currentUser.displayName}!</h2>
     <div className="messagebox">
         Messages wil be Here
+        {messageList.map((item)=> (
+            <div style={{margin: '10px'}}>
+            <p>{item.name}</p>
+            <h3>{item.message}</h3>
+            </div>
+        ))}
     </div>
-        <input placeholder="Enter Your Messages Here" />
-        <button>Send</button>
+        <input value = {message}placeholder="Enter Your Messages Here" onChange={(e) => setMessage(e.target.value)}/>
+        <button onClick={SendMessage}>Send</button>
         <button onClick={singOut}>LogOut</button>
   
     
